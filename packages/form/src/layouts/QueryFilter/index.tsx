5271b186356330bf4ea88064aa82849268b18ee7
sunnan5--24 <2454536294@qq.com>
2024-02-04 16:43:51 +0800

    feat:优化from样式

--- packages/form/src/layouts/QueryFilter/index.tsx
+++ packages/form/src/layouts/QueryFilter/index.tsx
@@ -4,7 +4,7 @@ import { type ProColumns } from "@antd-vc/pro-table";
 import { Button, Card, Col, DatePicker, Form, Input, Row, Select, Space } from "ant-design-vue";
 import type { FormInstance } from "ant-design-vue/es/form";
 import { cloneDeep } from "lodash-es";
-import { computed, defineComponent, ref, type PropType, type Ref } from "vue";
+import { computed, defineComponent, ref, type PropType } from "vue";
 import { useWindowWidth } from "./hooks";
 import { formColConfig, getSpanConfig } from "./utils";
 
@@ -38,16 +38,17 @@ export default defineComponent({
       default: false,
     },
     formState: {
-      type: Object as PropType<Ref<Record<string, any>>>,
+      type: Object as PropType<Record<string, any>>,
+      default: () => ({}),
       required: true,
     },
   },
   setup(props) {
-    const isCollapsed = ref(true);
+    const isCollapsed = ref(false);
     const formRef = ref<FormInstance>();
     const formState = props.formState || ref<Record<string, any>>({});
     const width = useWindowWidth();
-    const labelWidth = ref("90");
+    const labelWidth = ref("80" || "auto");
     const searchArr = computed<ProColumns[]>(() => {
       return (
         // 深拷贝一下是为了防止修改props，以后再解决复用问题；
@@ -76,7 +77,7 @@ export default defineComponent({
       const currentSpan = shownSearchArr.value.reduce((prev, cur) => {
         return prev + (cur.colSpan || 0);
       }, 0);
-      console.log("offset", currentSpan, spanSize.value);
+      console.log("offset", currentSpan % 24, spanSize.value);
       const offsetSpan = (currentSpan % 24) + spanSize.value.span;
       if (offsetSpan > 24) {
         return 24 - spanSize.value.span;
@@ -118,24 +119,24 @@ export default defineComponent({
         style={{ marginBottom: "16px" }}
         bodyStyle={{ padding: "16px 24px 0" }}
       >
-        <Form name="search" layout={spanSize.value.layout} model={formState.value} ref={formRef}>
+        <Form name="search" layout={spanSize.value.layout} model={formState} ref={formRef}>
           <Row gutter={24} justify="start">
             {shownSearchArr.value.map((item: any) => (
               <Col span={item.colSpan} key={item.dataIndex}>
                 <Form.Item
                   label={item.title}
                   name={item.dataIndex as string}
-                  // {...formItemFixStyle.value}
+                  {...formItemFixStyle.value}
                 >
                   {item.renderFormItem ? (
                     item.renderFormItem(undefined, {
-                      modelRef: formState.value, //透传表单对象和字段，使父组件可以双向绑定
+                      modelRef: formState, //透传表单对象和字段，使父组件可以双向绑定
                       fields: item.dataIndex,
                       placeholder: `请选择${item.title}`,
                     })
                   ) : typeof item.search === "object" && item.search?.options ? (
                     <Select
-                      v-model:value={formState.value[item.dataIndex]}
+                      v-model:value={formState[item.dataIndex]}
                       options={item.search.options.value}
                       showSearch={true}
                       placeholder={`请选择${item.title}`}
@@ -143,7 +144,7 @@ export default defineComponent({
                     />
                   ) : item.valueEnum ? (
                     <Select
-                      v-model:value={formState.value[item.dataIndex]}
+                      v-model:value={formState[item.dataIndex]}
                       options={Object.keys(item.valueEnum).map((i) => ({
                         value: i,
                         label:
@@ -156,13 +157,13 @@ export default defineComponent({
                     />
                   ) : item.valueType === "dateTime" ? (
                     <DatePicker
-                      v-model:value={formState.value[item.dataIndex]}
+                      v-model:value={formState[item.dataIndex]}
                       placeholder={`请输入${item.title}`}
                       allowClear
                     />
                   ) : (
                     <Input
-                      v-model:value={formState.value[item.dataIndex]}
+                      v-model:value={formState[item.dataIndex]}
                       placeholder={`请输入${item.title}`}
                       allowClear
                     />
@@ -181,8 +182,9 @@ export default defineComponent({
                     <Button
                       onClick={() => {
                         formRef.value?.resetFields();
-                        Object.keys(formState.value).forEach((item) => {
-                          formState.value[item] = undefined;
+                        console.log(formState.value, "formState.value");
+                        Object.keys(formState).forEach((item) => {
+                          formState[item] = undefined;
                         });
                         props.useFetchData?.();
                       }}
@@ -195,7 +197,7 @@ export default defineComponent({
                         // 查询的前置条件
                         if (props.lookUpCondition) {
                           // 把modelRef传出去
-                          props.lookUpCondition(formState.value).then((res: boolean) => {
+                          props.lookUpCondition(formState).then((res: boolean) => {
                             console.log("查询条件res", res);
                             // 成立才执行
                             if (res) {
@@ -212,7 +214,7 @@ export default defineComponent({
                       {props.textSearch ? props.textSearch : "查询"}
                     </Button>
                     {typeof props.search === "object" &&
-                      props.search?.optionRender(undefined, { modelRef: formState.value })}
+                      props.search?.optionRender(undefined, { modelRef: formState })}
                   </Space>
                   <a
                     onClick={() => {
